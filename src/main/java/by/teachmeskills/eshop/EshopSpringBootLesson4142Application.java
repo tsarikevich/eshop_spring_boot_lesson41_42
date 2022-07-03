b2ebebbbefc976ad885ae558db3e8d1ca643cd2b
tsarikevich <ma.tsarikevich@gmail.com>
2022-07-03 19:50:24 +0300

    add new project

---
+++ src/main/java/by/teachmeskills/eshop/EshopSpringBootLesson4142Application.java
@@ -0,0 +1,13 @@
+package by.teachmeskills.eshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EshopSpringBootLesson4142Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EshopSpringBootLesson4142Application.class, args);
+    }
+
+}
---
+++ src/test/java/by/teachmeskills/eshop/EshopSpringBootLesson4142ApplicationTests.java
@@ -0,0 +1,13 @@
+package by.teachmeskills.eshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EshopSpringBootLesson4142ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
